1a53d247202ce7633227b091e4e3357c317767cb
DuyT1notFum <duynek110@gmail.com>
2024-11-04 19:30:54 +0700

    Website Sales

---
+++ admin/src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react' // eslint-disable-line no-unused-vars
+import Navbar from './Components/Navbar/Navbar'
+import Admin from './Pages/Admin/Admin'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Admin/>
+    </div>
+  )
+}
+
+export default App
